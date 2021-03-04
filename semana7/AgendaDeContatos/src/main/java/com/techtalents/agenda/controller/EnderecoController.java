92287d310123d2d3ccc725ec7cfcf7fa1c5fd938
Anderson Rocha <anderson.rocha@addesistemas.com.br>
2021-03-03 21:46:30 -0300

    (feat) Finalizado CRUD do endereço aula semana 7.

--- semana7/AgendaDeContatos/src/main/java/com/techtalents/agenda/controller/EnderecoController.java
+++ semana7/AgendaDeContatos/src/main/java/com/techtalents/agenda/controller/EnderecoController.java
@@ -1,12 +1,23 @@
 package com.techtalents.agenda.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.techtalents.agenda.dto.input.EnderecoDtoInput;
+import com.techtalents.agenda.dto.output.EnderecoDtoOutput;
 import com.techtalents.agenda.service.EnderecoService;
 
 @RestController
@@ -16,14 +27,30 @@ public class EnderecoController {
 	@Autowired
 	private EnderecoService enderecoService;
 
-	@RequestMapping
-	private String get() {
-		return "Teste get Endereço";
+	@GetMapping(produces = "application/json")
+	public List<EnderecoDtoOutput> get() {
+		return this.enderecoService.getAll();
+	}
+
+	@GetMapping(path = "/{id}", produces = "application/json")
+	public EnderecoDtoOutput get(@PathVariable Long id) {
+		return this.enderecoService.get(id);
 	}
 
 	@PostMapping(consumes = "application/json", produces = "application/json")
-	public EnderecoDtoInput post(@RequestBody EnderecoDtoInput enderecoDtoInput) {
+	public EnderecoDtoOutput post(@RequestBody EnderecoDtoInput enderecoDtoInput) {
 		return this.enderecoService.create(enderecoDtoInput);
+	}
+
+	@PutMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+	public EnderecoDtoOutput put(@Valid @RequestBody EnderecoDtoInput enderecoDtoInput, @PathVariable Long id) {
+		return this.enderecoService.update(enderecoDtoInput, id);
+	}
+
+	@DeleteMapping(path = "/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		this.enderecoService.delete(id);
 	}
 
 }
--- semana7/AgendaDeContatos/src/main/java/com/techtalents/agenda/service/EnderecoService.java
+++ semana7/AgendaDeContatos/src/main/java/com/techtalents/agenda/service/EnderecoService.java
@@ -1,5 +1,8 @@
 package com.techtalents.agenda.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +25,30 @@ public class EnderecoService {
 		Endereco endereco = enderecoConverter.dtoToModel(enderecoDtoInput);
 		this.enderecoRepository.save(endereco);
 		return this.enderecoConverter.modelToDto(endereco);
+	}
+
+	public EnderecoDtoOutput update(EnderecoDtoInput enderecoDtoInput, Long id) {
+		Optional<Endereco> enderecoOptional = this.enderecoRepository.findById(id);
+		Endereco endereco = enderecoOptional.get();
+
+		this.enderecoConverter.dtoToModel(enderecoDtoInput, endereco);
+		this.enderecoRepository.save(endereco);
+		return this.enderecoConverter.modelToDto(endereco);
+
+	}
+
+	public List<EnderecoDtoOutput> getAll() {
+		return this.enderecoConverter.modelToDto(this.enderecoRepository.findAll());
+	}
+
+	public EnderecoDtoOutput get(Long id) {
+		Optional<Endereco> enderecoOptional = this.enderecoRepository.findById(id);
+		Endereco endereco = enderecoOptional.get();
+		return this.enderecoConverter.modelToDto(endereco);
+	}
+
+	public void delete(Long id) {
+		this.enderecoRepository.deleteById(id);
 	}
 
 }
