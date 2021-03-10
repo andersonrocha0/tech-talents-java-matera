a4f04aba2da88c6126d393d6c5fd6d19e9653ca4
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-10 13:21:38 -0300

    Adicionada anotação @Repository

--- semana7/AgendaDeContatos/src/main/java/com/techtalents/agenda/repository/ContatoRepository.java
+++ semana7/AgendaDeContatos/src/main/java/com/techtalents/agenda/repository/ContatoRepository.java
@@ -1,9 +1,11 @@
 package com.techtalents.agenda.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.techtalents.agenda.entity.Contato;
 
+@Repository
 public interface ContatoRepository extends JpaRepository<Contato, Long> {
 
 }
